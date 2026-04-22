6eaaa9a9a4e2f7941193d5de0b53cf36790afad6
Shyam Bisht <bishtshyam135@gmail.com>
2026-04-22 23:09:05 +0100

    add fix

--- 03-security-and-secrets/app/app.py
+++ 03-security-and-secrets/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "App is running.."
+    return "App is running...."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
