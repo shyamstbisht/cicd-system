61c31ba5b1fe6ff242cb1f2ce84e8195a7a68ec6
Shyam Bisht <bishtshyam135@gmail.com>
2026-04-22 23:40:58 +0100

    fixed code

--- 03-security-and-secrets/app/app.py
+++ 03-security-and-secrets/app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Api__key: {os.getenv('API_KEY')}"
+    return "Application running with secrets managements."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
